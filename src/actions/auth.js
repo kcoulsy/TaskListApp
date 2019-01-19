076e13fee5f4140c38a59c61f86d436348ecc83d
Kristian <kristian@coulsy.co.uk>
2019-01-19 12:27:06 +0000

    [LOGIN] updated startLogin action to use axios to post /users/login and dispatch res

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const login = (data) => {
 	return {
 		type: 'LOGIN',
@@ -8,6 +10,20 @@ export const login = (data) => {
 export const startLogin = (data) => {
 	return (dispatch) => {
 		// do api call then dispatch
-		return dispatch(login(data));
+		axios({
+			method: 'post',
+			url: '/users/login',
+			data
+		}).then((res) => {
+			if (res.data.tokens.length) {
+				return dispatch(login({
+					token: res.data.tokens[0].token,
+					user: {
+						username: res.data.username,
+						email: res.data.email
+					}
+				}));
+			}
+		}).catch((e) => console.log(e));
 	}
 }
