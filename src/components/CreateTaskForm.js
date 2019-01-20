b37c15cf3ef5d4893353f7a8660c4a4215c699b8
Kristian <kristian@coulsy.co.uk>
2019-01-20 13:54:32 +0000

    [CreateTaskForm] Added component to create new task item

---
+++ src/components/CreateTaskForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const FormButtonContainer = styled.div`
+width: 100%;
+padding: 10px 0;
+display: flex;
+justify-content: flex-end`;
+
+class CreateTaskForm extends Component {
+	state = {
+		title: '',
+		description: ''
+	}
+
+    handleChange = (ev) => {
+        const target = ev.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    createTask = () => {
+		const data = this.state;
+		// TODO better validation
+		if (this.state.title && this.state.description) {
+			axios({
+				method: 'post',
+				url: '/task',
+				headers: {
+					'x-auth': this.props.userToken
+				},
+				data
+			}).then((res) => {
+				if (res.data && this.props.onCreate) this.props.onCreate(res.data);
+			})
+		}
+    }
+	render() {
+		return (
+			<div className="form-group">
+				<label className="form-label" htmlFor="create-task-form-title">Title</label>
+				<input
+					name="title"
+					className="form-input"
+					type="text"
+					id="create-task-form-title"
+					placeholder="Title"
+					onChange={this.handleChange} />
+				<label className="form-label" htmlFor="create-task-form-desc">Description</label>
+				<textarea
+					name="description"
+					className="form-input"
+					id="create-task-form-desc"
+					placeholder="Description"
+					rows="3"
+					onChange={this.handleChange} />
+				<FormButtonContainer>
+					<button className="btn btn-primary" onClick={this.createTask}>Create Task</button>
+				</FormButtonContainer>
+			</div>
+		)
+	}
+};
+
+const mapStateToProps = state => ({
+	userToken: state.token
+})
+
+export default connect(mapStateToProps)(CreateTaskForm);
