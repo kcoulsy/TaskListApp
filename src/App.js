c2e6a3b89d02a015eb69349309d5aa96998a2c0d
Kristian <kristian@coulsy.co.uk>
2019-01-20 11:47:56 +0000

    [STORE] Updated store config to use redux-persist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import AppRouter from './routers/AppRouter';
 import configStore from './store/config';
 
 import '../node_modules/spectre.css/dist/spectre.css';
-const store = configStore();
+const {store, persistor} = configStore();
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppRouter />
+		<PersistGate loading={null} persistor={persistor}>
+			<AppRouter />
+		</PersistGate>
       </Provider>
     );
   }
--- src/store/config.js
+++ src/store/config.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
 // reducers import
@@ -7,9 +9,19 @@ import authReducer from '../reducers/auth';
 // required for redux dev tools in chrome
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// config for redux-persist
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
 export default () => {
-	return createStore(
-		authReducer,
+	let store = createStore(
+		persistedReducer,
 		composeEnhancers(applyMiddleware(thunk))
 	);
+	let persistor = persistStore(store);
+	return { store, persistor };
 }
