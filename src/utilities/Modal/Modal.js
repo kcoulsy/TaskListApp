b58f9694bb995ce5c74f0cb1b99a056402519f6c
Kristian <kristian@coulsy.co.uk>
2019-03-09 14:05:16 +0000

    FE: [Utilities] Wrapped Modal Component in Portal component

--- src/utilities/Modal/Modal.js
+++ src/utilities/Modal/Modal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Portal from '../Portal/Portal';
+
 class Modal extends Component {
   handleClose = ev => {
     ev.preventDefault();
@@ -8,29 +10,31 @@ class Modal extends Component {
 
   render() {
     return (
-      <div className={`modal ${this.props.active && 'active'}`} id="modal-id">
-        <a
-          onClick={this.handleClose}
-          href="#close"
-          className="modal-overlay"
-          aria-label="Close"
-        />
-        <div className="modal-container">
-          <div className="modal-header">
-            <a
-              onClick={this.handleClose}
-              href="#close"
-              className="btn btn-clear float-right"
-              aria-label="Close"
-            />
-            <div className="modal-title h5">{this.props.title}</div>
+      <Portal>
+        <div className={`modal ${this.props.active && 'active'}`} id="modal-id">
+          <a
+            onClick={this.handleClose}
+            href="#close"
+            className="modal-overlay"
+            aria-label="Close"
+          />
+          <div className="modal-container">
+            <div className="modal-header">
+              <a
+                onClick={this.handleClose}
+                href="#close"
+                className="btn btn-clear float-right"
+                aria-label="Close"
+              />
+              <div className="modal-title h5">{this.props.title}</div>
+            </div>
+            <div className="modal-body">
+              <div className="content">{this.props.body}</div>
+            </div>
+            <div className="modal-footer">{this.props.footer}</div>
           </div>
-          <div className="modal-body">
-            <div className="content">{this.props.body}</div>
-          </div>
-          <div className="modal-footer">{this.props.footer}</div>
         </div>
-      </div>
+      </Portal>
     );
   }
 }
